a613c023f22d45c6e14534049d59f9d0f4bd8b88
13620613948@163.com <729399ren>
2019-04-27 20:34:25 +0800

    algorithms->reverse integer

---
+++ src/main/algorithms/easy/ReverseInteger.java
@@ -0,0 +1,70 @@
+package src.main.algorithms.easy;
+
+/**
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ *
+ * Example 1:
+ * Input: 123
+ * Output: 321
+ *
+ * Example 2:
+ * Input: -123
+ * Output: -321
+ *
+ * Example 3:
+ * Input: 120
+ * Output: 21
+ *
+ * Note:
+ * Assume we are dealing with an environment
+ * which could only store integers
+ * within the 32-bit signed integer range: [−2^31,  2^31 − 1].
+ * For the purpose of this problem,
+ * assume that your function returns 0 when the reversed integer overflows.
+ */
+public class ReverseInteger {
+
+    /**
+     * reverse by string
+     * @param x
+     * @return
+     */
+    public int reverse1(int x) {
+        int result = 0;
+
+        String inputStr = String.valueOf(x);
+        if (x < 0) {
+            inputStr = inputStr.substring(1);
+        }
+
+        StringBuffer resultStr = new StringBuffer();
+        for (int index = inputStr.length() - 1; index >= 0; index--) {
+            resultStr.append(inputStr.charAt(index));
+        }
+        if (x < 0) {
+            resultStr.insert(0, "-");
+        }
+
+        try {
+            result = Integer.parseInt(resultStr.toString());
+        } catch (NumberFormatException e) {
+            result = 0;
+        }
+        return result;
+    }
+
+    public int reverse2(int x) {
+        long result = 0;
+
+        while (x != 0) {
+            result = result * 10 + x % 10;
+            x = x / 10;
+
+            if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+                return 0;
+            }
+        }
+
+        return (int) result;
+    }
+}
