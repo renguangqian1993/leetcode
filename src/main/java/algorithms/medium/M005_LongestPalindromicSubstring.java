1246950f80f043f7ffb85ae62d64d338059037a2
13620613948@163.com <729399ren>
2019-07-02 00:30:40 +0800

    算法->005，最长回文子串，官方解法：中心扩展法

--- src/main/java/algorithms/medium/M005_LongestPalindromicSubstring.java
+++ src/main/java/algorithms/medium/M005_LongestPalindromicSubstring.java
@@ -59,86 +59,36 @@ public class M005_LongestPalindromicSubstring {
         return judgePalindromeStrByRecurrence(inputStr.substring(1, inputStr.length() - 1));
     }
 
-    public static void main(String[] args) {
-        String str = "aabad";
-        String result = new M005_LongestPalindromicSubstring().longestPalindrome2(str);
-        System.out.println(result);
-    }
-
     /**
      * 官方解法：中心扩展法
      * 时间复杂度：O(n^2)，由于围绕中心来扩展回文会耗去 O(n)的时间，所以总的复杂度为 O(n^2)
      * 空间复杂度：O(1)。
+     * TODO start, end的判断不熟悉
      * @param s
      * @return
      */
     public String longestPalindrome2(String s) {
-        String longestPalindromeStr = "";
-
-        for (int index = 0; index < s.length() - 1; index++) {
-            int len1 = expandAroundCenter(s, index, index);
-            int len2 = expandAroundCenter(s, index, index + 1);
-
-            if (longestPalindromeStr.length() >= len1 && longestPalindromeStr.length() >= len2) {
-                continue;
-            }
-
-            int indexL;
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
             int len = Math.max(len1, len2);
-            if (len1 > len2) {
-                indexL = index - (len1 / 2);
-            } else {
-                indexL = index - (len2 / 2 - 1);
-            }
-            longestPalindromeStr = s.substring(indexL, len);
-
-        }
-        return longestPalindromeStr;
-    }
-
-    //此方法有问题
-    private int expandAroundCenter(String s, int indexL, int indexR) {
-        if (s.charAt(indexL) != s.charAt(indexR)) {
-            return 0;
-        }
-
-        int indexLTmp = indexL;
-        int indexRTmp = indexR;
-
-        while (s.charAt(indexLTmp) == s.charAt(indexRTmp) && indexLTmp > 0 && indexRTmp < s.length() - 1) {
-            indexLTmp--;
-            indexRTmp++;
-        }
-
-        return indexRTmp - indexLTmp + 1;
-    }
-
-    public String longestPalindrome3(String s) {
-        int len = s.length();
-        if (len <= 1) {
-            return s;
-        }
-        int longestPalindrome = 1;
-        String longestPalindromeStr = s.substring(0, 1);
-        boolean[][] dp = new boolean[len][len];
-
-        for (int r = 1; r < len; r++) {
-            for (int l = 0; l < r; l++) {
-                // 区间应该慢慢放大
-                // 状态转移方程：如果头尾字符相等并且中间也是回文
-                // 在头尾字符相等的前提下，如果收缩以后不构成区间（最多只有 1 个元素），直接返回 True 即可
-                // 否则要继续看收缩以后的区间的回文性
-                // 重点理解 or 的短路性质在这里的作用
-                if (s.charAt(l) == s.charAt(r) && (r - l <= 2 || dp[l + 1][r - 1])) {
-                    dp[l][r] = true;
-                    if (r - l + 1 > longestPalindrome) {
-                        longestPalindrome = r - l + 1;
-                        longestPalindromeStr = s.substring(l, r + 1);
-                    }
-                }
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
             }
         }
-        return longestPalindromeStr;
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
     }
 
 }
