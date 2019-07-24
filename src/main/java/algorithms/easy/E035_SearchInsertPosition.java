cd9a19886f8d964f7cdafc25878cde966ee9b27e
13620613948@163.com <729399ren>
2019-07-24 09:12:06 +0800

    算法->035，搜索插入位置

---
+++ src/main/java/algorithms/easy/E035_SearchInsertPosition.java
@@ -0,0 +1,38 @@
+package algorithms.easy;
+
+/**
+ * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ * <p>
+ * You may assume no duplicates in the array.
+ * <p>
+ * Example 1:
+ * Input: [1,3,5,6], 5
+ * Output: 2
+ * <p>
+ * Example 2:
+ * Input: [1,3,5,6], 2
+ * Output: 1
+ * <p>
+ * Example 3:
+ * Input: [1,3,5,6], 7
+ * Output: 4
+ * <p>
+ * Example 4:
+ * Input: [1,3,5,6], 0
+ * Output: 0
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-insert-position
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E035_SearchInsertPosition {
+    /**
+     * @param nums
+     * @param target
+     * @return
+     * @idea 二分查找法
+     */
+    public int searchInsert(int[] nums, int target) {
+
+    }
+}
