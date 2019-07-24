388272fe77d42ccdf66ef65f9a17f44a75dbfb35
renguangqian <13620613948@163.com>
2019-07-24 21:28:26 +0800

    算法->035，二分查找

--- src/main/java/algorithms/easy/E035_SearchInsertPosition.java
+++ src/main/java/algorithms/easy/E035_SearchInsertPosition.java
@@ -27,12 +27,43 @@ package algorithms.easy;
  */
 public class E035_SearchInsertPosition {
     /**
-     * @param nums
-     * @param target
-     * @return
-     * @idea 二分查找法
+     * 执行用时:1 ms, 在所有 Java 提交中击败了90.00%的用户
+     * 内存消耗:39.2 MB, 在所有 Java 提交中击败了47.83%的用户
+     * @idea 二分查找 + 递归
      */
     public int searchInsert(int[] nums, int target) {
+        if (null == nums || nums.length == 0) {
+            return 0;
+        }
+        
+        return findIndex(nums, 0, nums.length - 1, target);
+    }
+    
+    private int findIndex(int[] nums, int indexL, int indexR, int target) {
+        if (nums[indexL] >= target) {
+            return indexL;
+        } else if (nums[indexR] == target) {
+            return indexR;
+        } else if (nums[indexR] < target) {
+            return indexR + 1;
+        }
+        
+        int indexMid = (indexL + indexR) /2;
+        if (nums[indexMid] == target) {
+            return indexMid;
+        } else if (nums[indexMid] > target) {
+            return findIndex(nums, indexL, indexMid - 1, target);
+        } else {
+            return findIndex(nums, indexMid + 1, indexR, target);
+        }
+    }
+    
+    public static void main(String[] args) {
+        E035_SearchInsertPosition instance = new E035_SearchInsertPosition();
+        
+        int[] nums = new int[]{1,3};
+        int target = 3;
 
+        System.out.println(instance.searchInsert(nums, target));
     }
 }
