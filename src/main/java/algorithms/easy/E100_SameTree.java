d17bc356419fcfa946bd88e364c96e4a83d637e2
13620613948@163.com <729399ren>
2019-08-18 23:51:17 +0800

    100=>判断二叉树是否相等，官方解法2

--- src/main/java/algorithms/easy/E100_SameTree.java
+++ src/main/java/algorithms/easy/E100_SameTree.java
@@ -1,5 +1,7 @@
 package algorithms.easy;
 
+import java.util.ArrayDeque;
+
 /**
  * Given two binary trees, write a function to check if they are the same or not.
  * <p>
@@ -44,7 +46,13 @@ public class E100_SameTree {
      * @param p
      * @param q
      * @return
+     *
      * @description 使用递归，判断当前节点数值、左子树、右子树
+     *
+     * 时间复杂度 : O(N)，其中 N 是树的结点数，因为每个结点都访问一次。
+     *
+     * 空间复杂度 : 最优情况（完全平衡二叉树）时为 O(log(N))，最坏情况下（完全不平衡二叉树）时为 O(N)，用于维护递归栈。
+     *
      */
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (null == p && null == q) {
@@ -59,6 +67,68 @@ public class E100_SameTree {
                 && isSameTree(p.right, q.right);
     }
 
+    /**
+     * TODO 官方解法二
+     * 方法二：迭代
+     * 直觉
+     * <p>
+     * 从根开始，每次迭代将当前结点从双向队列中弹出。然后，进行方法一中的判断：
+     * <p>
+     * p 和 q 不是 None,
+     * <p>
+     * p.val 等于 q.val,
+     * <p>
+     * 若以上均满足，则压入子结点。
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/two-sum/solution/xiang-tong-de-shu-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param p
+     * @param q
+     * @return
+     */
+    public boolean check(TreeNode p, TreeNode q) {
+        // p and q are null
+        if (p == null && q == null) return true;
+        // one of p and q is null
+        if (q == null || p == null) return false;
+        if (p.val != q.val) return false;
+        return true;
+    }
+
+    public boolean isSameTree2(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (!check(p, q)) return false;
+
+        // init deques
+        ArrayDeque<TreeNode> deqP = new ArrayDeque<TreeNode>();
+        ArrayDeque<TreeNode> deqQ = new ArrayDeque<TreeNode>();
+        deqP.addLast(p);
+        deqQ.addLast(q);
+
+        while (!deqP.isEmpty()) {
+            p = deqP.removeFirst();
+            q = deqQ.removeFirst();
+
+            if (!check(p, q)) return false;
+            if (p != null) {
+                // in Java nulls are not allowed in Deque
+                if (!check(p.left, q.left)) return false;
+                if (p.left != null) {
+                    deqP.addLast(p.left);
+                    deqQ.addLast(q.left);
+                }
+                if (!check(p.right, q.right)) return false;
+                if (p.right != null) {
+                    deqP.addLast(p.right);
+                    deqQ.addLast(q.right);
+                }
+            }
+        }
+        return true;
+    }
 
     private class TreeNode {
         int val;
