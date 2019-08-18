bddf605e04dab66583b475bfb8806810ab78c924
13620613948@163.com <729399ren>
2019-08-19 00:05:36 +0800

    101=>判断二叉树是否左右对称

---
+++ src/main/java/algorithms/easy/E101_SymmetricTree.java
@@ -0,0 +1,107 @@
+package algorithms.easy;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ * <p>
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * / \ / \
+ * 3  4 4  3
+ *  
+ * <p>
+ * But the following [1,2,2,null,3,null,3] is not:
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * \   \
+ * 3    3
+ *  
+ * <p>
+ * Note:
+ * Bonus points if you could solve it both recursively and iteratively.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/symmetric-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E101_SymmetricTree {
+
+    /**
+     * 执行用时 :1 ms, 在所有 Java 提交中击败了99.67%的用户
+     * 内存消耗 :35.9 MB, 在所有 Java 提交中击败了79.48%的用户
+     *
+     * @param root
+     * @return
+     * @description 使用递归。。。
+     * 时间复杂度：O(n)，因为我们遍历整个输入树一次，所以总的运行时间为 O(n)，其中 n 是树中结点的总数。
+     * 空间复杂度：递归调用的次数受树的高度限制。在最糟糕情况下，树是线性的，其高度为 O(n)。因此，在最糟糕的情况下，由栈上的递归调用造成的空间复杂度为 O(n)。
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if (null == root) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (null == left && null == right) {
+            return true;
+        } else if (null == left || null == right) {
+            return false;
+        } else if (left.val != right.val) {
+            return false;
+        }
+
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+
+    /**
+     * TODO 官方解法2
+     * 除了递归的方法外，我们也可以利用队列进行迭代。
+     * 队列中每两个连续的结点应该是相等的，而且它们的子树互为镜像。
+     * 最初，队列中包含的是 root 以及 root。
+     * 该算法的工作原理类似于 BFS，但存在一些关键差异。每次提取两个结点并比较它们的值。
+     * 然后，将两个结点的左右子结点按相反的顺序插入队列中。
+     * 当队列为空时，或者我们检测到树不对称（即从队列中取出两个不相等的连续结点）时，该算法结束。
+     * <p>
+     * 时间复杂度：O(n)O(n)，因为我们遍历整个输入树一次，所以总的运行时间为 O(n)O(n)，其中 nn 是树中结点的总数。
+     * 空间复杂度：搜索队列需要额外的空间。在最糟糕情况下，我们不得不向队列中插入 O(n)O(n) 个结点。因此，空间复杂度为 O(n)O(n)。
+     *
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric2(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            if (t1 == null && t2 == null) continue;
+            if (t1 == null || t2 == null) return false;
+            if (t1.val != t2.val) return false;
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
+        }
+        return true;
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
