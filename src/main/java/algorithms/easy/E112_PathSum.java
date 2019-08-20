6cc0d4bc689172a30266e32fd7c21c31eb9475b0
renguangqian <13620613948@163.com>
2019-08-20 19:57:28 +0800

    112=>求二叉树路径总和为特定值的路径

--- src/main/java/algorithms/easy/E112_PathSum.java
+++ src/main/java/algorithms/easy/E112_PathSum.java
@@ -1,5 +1,9 @@
 package algorithms.easy;
 
+import javafx.util.Pair;
+
+import java.util.Stack;
+
 /**
  * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
  *
@@ -25,12 +29,50 @@ package algorithms.easy;
 public class E112_PathSum {
 
     /**
-     * @thought 1.深度优先搜索；2.广度优先搜索
+     * @description 1.深度优先搜索，利用stack的FIFO特性，
      * @param root
      * @param sum
      * @return
      */
-    public boolean hasPathSum(TreeNode root, int sum) {
+    public boolean hasPathSumDepthFirstSearch(TreeNode root, int sum) {
+        if (null == root) {
+            return false;
+        }
+
+        Stack<Pair<TreeNode, Integer>> stack = new Stack<>();
+        stack.push(new Pair<>(root, root.val));
+
+        while (!stack.isEmpty()) {
+            Pair<TreeNode, Integer> pair = stack.pop();
+            TreeNode node = pair.getKey();
+            Integer tmpSum = pair.getValue();
+            if (tmpSum == sum && node.left == null && node.right == null) {
+                return true;
+            } else if (tmpSum != sum) {
+                if (null != node.right) {
+                    stack.push(new Pair<>(node.right, tmpSum + node.right.val));
+                }
+                if (null != node.left) {
+                    stack.push(new Pair<>(node.left, tmpSum + node.left.val));
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        E112_PathSum task = new E112_PathSum();
+        task.hasPathSumDepthFirstSearch(null, -5);
+    }
+
+    /**
+     * @description 2.广度优先搜索
+     * @param root
+     * @param sum
+     * @return
+     */
+    public boolean hasPathSumBreadthFirstSearch(TreeNode root, int sum) {
         return false;
     }
 
