5d9c721503b92fd3caa2e9eec5e8dd739522a8d9
renguangqian <13620613948@163.com>
2019-08-20 11:04:54 +0800

    107=>二叉树转为两重list，并逆转

--- src/main/java/algorithms/easy/E107_BinaryTreeLevelOrderTraversal_II.java
+++ src/main/java/algorithms/easy/E107_BinaryTreeLevelOrderTraversal_II.java
@@ -1,6 +1,7 @@
 package algorithms.easy;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -22,10 +23,12 @@ import java.util.List;
  */
 public class E107_BinaryTreeLevelOrderTraversal_II {
     /**
-     * 使用递归，超出时间限制了
+     * 执行用时 :2 ms, 在所有 Java 提交中击败了97.82%的用户
+     * 内存消耗 :37.4 MB, 在所有 Java 提交中击败了28.29%的用户
      *
-     * @param root
-     * @return
+     * @description 还是使用递归，最外层创建大list，每一层进行补充，结束之后进行逆转
+     *
+     * 
      */
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
         List<List<Integer>> list = new ArrayList<>();
@@ -33,31 +36,30 @@ public class E107_BinaryTreeLevelOrderTraversal_II {
             return list;
         }
 
+        this.fillUpList(list, root, 0);
 
-        List<List<Integer>> leftList = levelOrderBottom(root.left);
-        List<List<Integer>> rightList = levelOrderBottom(root.right);
-
-        int maxDepth = Math.max(leftList.size(), rightList.size());
-        for (int index = maxDepth - 1; index >= 0; index++) {
-            List<Integer> listOfOneDepth = new ArrayList<>();
-            if (leftList.size() > index) {
-                listOfOneDepth.addAll(leftList.get(index));
-            }
-            if (rightList.size() > index) {
-                listOfOneDepth.addAll(rightList.get(index));
-            }
-
-            list.add(listOfOneDepth);
-        }
-
-
-        list.add(new ArrayList<Integer>() {{
-            add(root.val);
-        }});
+        Collections.reverse(list);
 
         return list;
     }
 
+    private void fillUpList(List<List<Integer>> lists, TreeNode node, int depth) {
+        if (null == node) {
+            return;
+        }
+
+        List<Integer> datasOfThisDepth = Collections.emptyList();
+        if (lists.size() < depth + 1) {
+            datasOfThisDepth = new ArrayList<>();
+            lists.add(datasOfThisDepth);
+        } else {
+            datasOfThisDepth = lists.get(depth);
+        }
+
+        datasOfThisDepth.add(node.val);
+        fillUpList(lists, node.left, depth + 1);
+        fillUpList(lists, node.right, depth + 1);
+    }
 
 
 
