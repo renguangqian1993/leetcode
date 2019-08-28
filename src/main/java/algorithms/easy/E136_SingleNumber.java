2a31db5e1b27778e8a9c8b9b3bf0c4273536b567
renguangqian <13620613948@163.com>
2019-08-28 20:34:34 +0800

    136，若干排序算法

--- src/main/java/algorithms/easy/E136_SingleNumber.java
+++ src/main/java/algorithms/easy/E136_SingleNumber.java
@@ -29,11 +29,147 @@ public class E136_SingleNumber {
         return 0;
     }
 
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{1,3,5,7,9,2,4,6,8,10,-1};
+
+        sort4(nums);
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+
     /**
-     * 排序算法
-     * @param nums
+     * 冒泡排序
      */
-    private static void sort(int[] nums) {
+    private static void sort1(int[] nums) {
+        for (int leftIndexOfUnsorted = 0; leftIndexOfUnsorted < nums.length; leftIndexOfUnsorted++) {
+            for (int index = nums.length - 1; index > leftIndexOfUnsorted; index--) {
+                if (nums[index] < nums[index - 1]) {
+                    int tmp = nums[index];
+                    nums[index] = nums[index - 1];
+                    nums[index - 1] = tmp;
+                }
+            }
+        }
+    }
+
+    /**
+     * 选择排序
+     */
+    private static void sort2(int[] nums) {
+        for (int leftIndexOfUnsorted = 0; leftIndexOfUnsorted < nums.length - 1; leftIndexOfUnsorted++) {
+
+            int minIndex = leftIndexOfUnsorted;
+
+            for (int index = leftIndexOfUnsorted + 1; index < nums.length; index++) {
+                if (nums[minIndex] > nums[index]) {
+                    minIndex = index;
+                }
+            }
+
+            int tmp = nums[leftIndexOfUnsorted];
+            nums[leftIndexOfUnsorted] = nums[minIndex];
+            nums[minIndex] = tmp;
+        }
+    }
+
+    /**
+     * 插入排序
+     */
+    private static void sort3(int[] nums) {
+        for (int indexL = 0; indexL < nums.length - 1; indexL++) {
+
+            int dataToInsert = nums[indexL + 1];
+
+            for (int indexToInsert = indexL; indexToInsert >= 0; indexToInsert--) {
+                if (nums[indexToInsert] > dataToInsert) {
+                    nums[indexToInsert + 1] = nums[indexToInsert];
+                } else {
+                    nums[indexToInsert + 1] = dataToInsert;
+
+                    break;
+                }
+            }
+
+        }
+    }
+    /**
+     * 希尔排序
+     * TODO 不会写！！！
+     */
+    private static void sort4(int[] nums) {
+        int len = nums.length;
+        int temp, gap = len / 2;
+        while (gap > 0) {
+            for (int i = gap; i < len; i++) {
+                temp = nums[i];
+                int preIndex = i - gap;
+                while (preIndex >= 0 && nums[preIndex] > temp) {
+                    nums[preIndex + gap] = nums[preIndex];
+                    preIndex -= gap;
+                }
+                nums[preIndex + gap] = temp;
+            }
+            gap /= 2;
+        }
+    }
+    /**
+     * 归并排序
+     */
+    private static void sort5(int[] nums) {
+
+    }
+    private static void sort5_1(int[] nums, int indexL, int indexC, int indexR) {
+        if (indexL == indexR) {
+            return;
+        } else if (indexL + 2 == indexR) {
+            if (nums[indexL] > nums[indexR]) {
+                int tmp = nums[indexL];
+                nums[indexL] = nums[indexR];
+                nums[indexR] = tmp;
+            }
+        }
+        int indexLC = (indexL + indexC) / 2;
+        sort5_1(nums, indexL, indexLC, indexC);
+
+        int indexRC = (indexR + indexC) / 2;
+        sort5_1(nums, indexC + 1, indexRC, indexR);
+
+        
+
+    }
+
+    /**
+     * 快速排序
+     */
+    private static void sort6(int[] nums) {
+
+    }
+    /**
+     * 堆排序
+     */
+    private static void sort7(int[] nums) {
+
+    }
+    /**
+     * 计数排序
+     */
+    private static void sort8(int[] nums) {
+
+    }
+    /**
+     * 桶排序
+     */
+    private static void sort9(int[] nums) {
+
+    }
+    /**
+     * 基数排序
+     */
+    private static void sort10(int[] nums) {
 
     }
 }
