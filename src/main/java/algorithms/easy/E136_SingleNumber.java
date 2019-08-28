506ccc1c79860e8f3f0393761bab3222fe4dded3
renguangqian <13620613948@163.com>
2019-08-28 12:30:48 +0800

    E136=>找出数组中只出现一次的元素

---
+++ src/main/java/algorithms/easy/E136_SingleNumber.java
@@ -0,0 +1,39 @@
+package algorithms.easy;
+
+/**
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ *
+ * Note:
+ *
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ *
+ * Example 1:
+ * Input: [2,2,1]
+ * Output: 1
+ *
+ * Example 2:
+ * Input: [4,1,2,1,2]
+ * Output: 4
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/single-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E136_SingleNumber {
+    /**
+     * 将数组排序，从下标0开始找出第一个与其后一个元素不相等的元素
+     * @param nums
+     * @return
+     */
+    public int singleNumber(int[] nums) {
+        return 0;
+    }
+
+    /**
+     * 排序算法
+     * @param nums
+     */
+    private static void sort(int[] nums) {
+
+    }
+}
