00bc5b8cbc892245efbdf383e3063d4ca7d8d787
13620613948@163.com <729399ren>
2019-09-04 23:17:27 +0800

    256=>动态规划，不会

---
+++ src/main/java/algorithms/easy/E256_PaintHouse.java
@@ -0,0 +1,26 @@
+package algorithms.easy;
+
+/**
+ * There are a row of n houses, each house can be painted with one of the three colors: red, blue or green. The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
+ * <p>
+ * The cost of painting each house with a certain color is represented by a n x 3 cost matrix. For example, costs[0][0] is the cost of painting house 0 with color red; costs[1][2] is the cost of painting house 1 with color green, and so on... Find the minimum cost to paint all houses.
+ * <p>
+ * Note:
+ * All costs are positive integers.
+ * <p>
+ * Example:
+ * <p>
+ * Input: [[17,2,17],[16,16,5],[14,3,19]]
+ * Output: 10
+ * Explanation: Paint house 0 into blue, paint house 1 into green, paint house 2 into blue.
+ *              Minimum cost: 2 + 5 + 3 = 10.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/paint-house
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E256_PaintHouse {
+    public static int minCost(int[][] costs) {
+        return 0;
+    }
+}
