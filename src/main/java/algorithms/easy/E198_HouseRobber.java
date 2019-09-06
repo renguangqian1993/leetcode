b8c4942476be32af09db8b4004eb4b36ed14f454
renguangqian <13620613948@163.com>
2019-09-06 16:18:54 +0800

    198，动态规划

--- src/main/java/algorithms/easy/E198_HouseRobber.java
+++ src/main/java/algorithms/easy/E198_HouseRobber.java
@@ -40,4 +40,27 @@ public class E198_HouseRobber {
         }
         return currMax;
     }
+
+    /**
+     * TODO 参考官方，需要再来一次哦
+     * 执行用时 :1 ms, 在所有 Java 提交中击败了96.04%的用户
+     * 内存消耗 :34.2 MB, 在所有 Java 提交中击败了86.99%的用户
+     */
+    public static int rob2(int[] nums) {
+        //sum(n-1) or (sum(n-2)+currNum)
+        int currMax = 0;
+
+        //sum(n-1)
+        int prevMax = 0;
+
+        for (int num : nums) {
+            int tmp = currMax;
+
+            currMax = Math.max(prevMax + num, currMax);
+
+            prevMax = tmp;
+        }
+
+        return currMax;
+    }
 }
