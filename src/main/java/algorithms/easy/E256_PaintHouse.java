b61903574a03a24faddc09f7e49add9b225fead7
renguangqian <13620613948@163.com>
2019-09-06 18:10:19 +0800

    256，动态规划

--- src/main/java/algorithms/easy/E256_PaintHouse.java
+++ src/main/java/algorithms/easy/E256_PaintHouse.java
@@ -1,5 +1,7 @@
 package algorithms.easy;
 
+import java.util.Arrays;
+
 /**
  * There are a row of n houses, each house can be painted with one of the three colors: red, blue or green. The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
  * <p>
@@ -20,7 +22,33 @@ package algorithms.easy;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class E256_PaintHouse {
+
+    /**
+     * TODO，动态规划，需要重做
+     * 执行用时 :2 ms, 在所有 Java 提交中击败了91.53%的用户
+     * 内存消耗 :37.2 MB, 在所有 Java 提交中击败了100.00%的用户
+     */
     public static int minCost(int[][] costs) {
-        return 0;
+        if (0 == costs.length) return 0;
+
+        int[] minCosts = new int[]{costs[0][0], costs[0][1], costs[0][2]};
+        int[] preMinCosts = new int[]{costs[0][0], costs[0][1], costs[0][2]};
+
+
+        for (int index = 1; index < costs.length; index++) {
+            minCosts[0] = Math.min(preMinCosts[1], preMinCosts[2]) + costs[index][0];
+            minCosts[1] = Math.min(preMinCosts[0], preMinCosts[2]) + costs[index][1];
+            minCosts[2] = Math.min(preMinCosts[0], preMinCosts[1]) + costs[index][2];
+
+            preMinCosts = Arrays.copyOf(minCosts, minCosts.length);
+        }
+
+        return Math.min(minCosts[0], Math.min(minCosts[1], minCosts[2]));
+    }
+
+    public static void main(String[] args) {
+//        int[][] costs = {{17,2,17},{16,16,5},{14,3,19}};
+        int[][] costs = {{3,5,3},{6,17,6},{7,13,18},{9,10,18}};
+        minCost(costs);
     }
 }
