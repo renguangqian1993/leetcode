36cf0bdfeed8babab57bd862581a63883273db10
renguangqian <13620613948@163.com>
2019-09-09 19:40:58 +0800

    258

--- src/main/java/algorithms/easy/E258_AddDigits.java
+++ src/main/java/algorithms/easy/E258_AddDigits.java
@@ -17,7 +17,22 @@ package algorithms.easy;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class E258_AddDigits {
+    /**
+     * 执行用时 :2 ms, 在所有 Java 提交中击败了98.25%的用户
+     * 内存消耗 :34.5 MB, 在所有 Java 提交中击败了11.84%的用户
+     */
     public int addDigits(int num) {
+        int result = num;
+        while (num >= 10) {
+            result = 0;
+            while (num > 0) {
+                result += num % 10;
+                num /= 10;
+            }
 
+            num = result;
+        }
+
+        return result;
     }
 }
