c539b732600cd12429a98fdc992e30e3570a2328
renguangqian <13620613948@163.com>
2019-09-09 19:57:57 +0800

    263

---
+++ src/main/java/algorithms/easy/E263_UglyNumber.java
@@ -0,0 +1,39 @@
+package algorithms.easy;
+
+/**
+ * Write a program to check whether a given number is an ugly number.
+ *
+ * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+ *
+ * Example 1:
+ * Input: 6
+ * Output: true
+ * Explanation: 6 = 2 × 3
+ *
+ * Example 2:
+ * Input: 8
+ * Output: true
+ * Explanation: 8 = 2 × 2 × 2
+ *
+ * Example 3:
+ * Input: 14
+ * Output: false
+ * Explanation: 14 is not ugly since it includes another prime factor 7.
+ *
+ * Note:
+ *
+ * 1 is typically treated as an ugly number.
+ * Input is within the 32-bit signed integer range: [−231,  231 − 1].
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/ugly-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E263_UglyNumber {
+    public boolean isUgly(int num) {
+
+        return ((num & 2) == 2 && (num & 1 ^ 1)  == 0)
+                || ((num & 3) == 3)
+                || (num & 5) == 5;
+    }
+}
