e8fce6e247d3f536e34deba7bad22e2fc9c88bdd
renguangqian <13620613948@163.com>
2019-09-21 18:17:34 +0800

    004，两有序数组中位数，官方解法

--- src/main/java/algorithms/difficult/D004_MedianOfTwoSortedArrays.java
+++ src/main/java/algorithms/difficult/D004_MedianOfTwoSortedArrays.java
@@ -54,4 +54,69 @@ public class D004_MedianOfTwoSortedArrays {
         return (sumArray[indexL] + sumArray[indexR]) / 2.0D;
     }
 
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{9, 10};
+        int[] nums2 = new int[]{1, 2, 3, 4, 5, 6, 7};
+        double result = findMedianSortedArrays2(nums1, nums2);
+        System.out.println(result);
+    }
+
+    /**
+     * TODO 官方解法
+     * 思路：下标初始化为第一个数组的中间位置，然后循环向前向后移动
+     * @param A
+     * @param B
+     * @return
+     */
+    public static double findMedianSortedArrays2(int[] A, int[] B) {
+
+        if (A.length > B.length) { // to ensure lengthA<=lengthB
+            int[] temp = A;
+            A = B;
+            B = temp;
+        }
+
+        int lengthA = A.length;
+        int lengthB = B.length;
+        int halfLen = (lengthA + lengthB + 1) / 2;
+
+        int iMin = 0;
+        int iMax = lengthA;
+
+        while (iMin <= iMax) {
+
+            int indexOfA = (iMin + iMax) / 2;
+            int indexOfB = halfLen - indexOfA;
+
+            if (indexOfA < iMax && B[indexOfB - 1] > A[indexOfA]) {
+                iMin = indexOfA + 1; // indexOfA is too small
+            } else if (indexOfA > iMin && A[indexOfA - 1] > B[indexOfB]) {
+                iMax = indexOfA - 1; // indexOfA is too big
+            } else { // indexOfA is perfect
+                int maxLeft = 0;
+                if (indexOfA == 0) {
+                    maxLeft = B[indexOfB - 1];
+                } else if (indexOfB == 0) {
+                    maxLeft = A[indexOfA - 1];
+                } else {
+                    maxLeft = Math.max(A[indexOfA - 1], B[indexOfB - 1]);
+                }
+                if ((lengthA + lengthB) % 2 == 1) {
+                    return maxLeft;
+                }
+
+                int minRight = 0;
+                if (indexOfA == lengthA) {
+                    minRight = B[indexOfB];
+                } else if (indexOfB == lengthB) {
+                    minRight = A[indexOfA];
+                } else {
+                    minRight = Math.min(B[indexOfB], A[indexOfA]);
+                }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+    }
 }
