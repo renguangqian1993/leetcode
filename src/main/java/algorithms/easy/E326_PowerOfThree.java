0c24d922be40cad3af198f18611e489b68f5d513
renguangqian <13620613948@163.com>
2019-09-23 21:31:54 +0800

    326，判断是否3的幂。1.基数转换即转换进制；2.最大的幂

---
+++ src/main/java/algorithms/easy/E326_PowerOfThree.java
@@ -0,0 +1,62 @@
+package algorithms.easy;
+
+/**
+ * Given an integer, write a function to determine if it is a power of three.
+ *
+ * Example 1:
+ * Input: 27
+ * Output: true
+ *
+ * Example 2:
+ * Input: 0
+ * Output: false
+ *
+ * Example 3:
+ * Input: 9
+ * Output: true
+ *
+ * Example 4:
+ * Input: 45
+ * Output: false
+ *
+ * Follow up:
+ * Could you do it without using any loop / recursion?
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/power-of-three
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E326_PowerOfThree {
+    /**
+     * 暴力解法
+     */
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+
+        return n == 1;
+    }
+
+    /**
+     * 将数组转换成以3进制的字符串，使用正则判断，todo，官方解法
+     * @param n
+     * @return
+     */
+    public boolean isPowerOfThree2(int n) {
+        String baseChange = Integer.toString(n, 3);
+        return baseChange.matches("^10*$");
+    }
+
+    /**
+     * maxPow是Integer.MAX_VALUE范围内，最大的3的幂，是否应该通过程序去计算得到而不是写死呢
+     */
+    public boolean isPowerOfThree3(int n) {
+        int maxPow = (int) Math.pow(3.0D, 19.0D);
+        return n > 0 && maxPow % n == 0;
+    }
+}
