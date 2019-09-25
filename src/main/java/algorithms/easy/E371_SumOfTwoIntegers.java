59044a9f6ac5363781a2b4d152c6bfc0ea20d9af
renguangqian <13620613948@163.com>
2019-09-25 21:35:04 +0800

    371，位运算

---
+++ src/main/java/algorithms/easy/E371_SumOfTwoIntegers.java
@@ -0,0 +1,23 @@
+package algorithms.easy;
+
+/**
+ * Calculate the sum of two integers a and b,
+ * but you are not allowed to use the operator + and -.
+ *
+ * Example 1:
+ * Input: a = 1, b = 2
+ * Output: 3
+ *
+ * Example 2:
+ * Input: a = -2, b = 3
+ * Output: 1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sum-of-two-integers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E371_SumOfTwoIntegers {
+    public int getSum(int a, int b) {
+        return 0;
+    }
+}
