780c79f2d8b40dab511c67722ded31af8a658a9b
13620613948@163.com <729399ren>
2019-09-26 08:02:58 +0800

    387

---
+++ src/main/java/algorithms/easy/E387_FirstUniqueCharacterInAString.java
@@ -0,0 +1,24 @@
+package algorithms.easy;
+
+/**
+ * Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+ * <p>
+ * Examples:
+ * <p>
+ * s = "leetcode"
+ * return 0.
+ * <p>
+ * s = "loveleetcode",
+ * return 2.
+ * <p>
+ * Note: You may assume the string contain only lowercase letters.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/first-unique-character-in-a-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E387_FirstUniqueCharacterInAString {
+    public int firstUniqChar(String s) {
+
+    }
+}
