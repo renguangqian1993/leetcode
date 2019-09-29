9cbb96183d1274642b4472f6624515901dfb70af
renguangqian <13620613948@163.com>
2019-09-29 16:05:58 +0800

    400

---
+++ src/main/java/algorithms/easy/E400_NthDigit.java
@@ -0,0 +1,43 @@
+package algorithms.easy;
+
+/**
+ * Find the nth digit of the infinite integer sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...
+ *
+ * Note:
+ * n is positive and will fit within the range of a 32-bit signed integer (n < 2^31).
+ *
+ * Example 1:
+ * Input:
+ * 3
+ * Output:
+ * 3
+ *
+ * Example 2:
+ * Input:
+ * 11
+ * Output:
+ * 0
+ *
+ * Explanation:
+ * The 11th digit of the sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ... is a 0, which is part of the number 10.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/nth-digit
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E400_NthDigit {
+    public int findNthDigit(int n) {
+        //当前处于几位数
+        int carry = 0;
+        //统计数字位个数
+        int countOfNum = 0;
+        while (countOfNum < n) {
+            carry++;
+
+            countOfNum += Math.pow(10.0D, carry - 1) * 9 * carry;
+        }
+        return 0;
+    }
+
+
+}
