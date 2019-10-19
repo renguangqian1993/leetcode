18df047e19829ca073126d96bb7971488756f9ba
13620613948@163.com <729399ren>
2019-10-20 02:32:20 +0800

    447

---
+++ src/main/java/algorithms/easy/E447_NumberOfBoomerangs.java
@@ -0,0 +1,30 @@
+package algorithms.easy;
+
+/**
+ * Given n points in the plane that are all pairwise distinct,
+ * a "boomerang" is a tuple of points (i, j, k) such that
+ * the distance between i and j equals the distance between i and k (the order of the tuple matters).
+ * <p>
+ * Find the number of boomerangs.
+ * You may assume that n will be at most 500 and coordinates of points are all in the range [-10000, 10000] (inclusive).
+ * <p>
+ * Example:
+ * <p>
+ * Input:
+ * [[0,0],[1,0],[2,0]]
+ * <p>
+ * Output:
+ * 2
+ * <p>
+ * Explanation:
+ * The two boomerangs are [[1,0],[0,0],[2,0]] and [[1,0],[2,0],[0,0]]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/number-of-boomerangs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E447_NumberOfBoomerangs {
+    public int numberOfBoomerangs(int[][] points) {
+        return 0;
+    }
+}
