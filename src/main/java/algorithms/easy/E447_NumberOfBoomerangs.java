d136b9d080ae6860de2bb8920a784d79492b3ffc
13620613948@163.com <729399ren>
2019-10-21 00:17:14 +0800

    447

--- src/main/java/algorithms/easy/E447_NumberOfBoomerangs.java
+++ src/main/java/algorithms/easy/E447_NumberOfBoomerangs.java
@@ -1,5 +1,8 @@
 package algorithms.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given n points in the plane that are all pairwise distinct,
  * a "boomerang" is a tuple of points (i, j, k) such that
@@ -24,7 +27,42 @@ package algorithms.easy;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class E447_NumberOfBoomerangs {
-    public int numberOfBoomerangs(int[][] points) {
-        return 0;
+    public static void main(String[] args) {
+//        numberOfBoomerangs(new int[][]{new int[]{0,0},new int[]{1,0},new int[]{2,0}});
+        numberOfBoomerangs(new int[][]{new int[]{0, 0}, new int[]{1, 0}, new int[]{-1, 0}, new int[]{0, 1}, new int[]{0, -1}});
+    }
+
+    /**
+     * TODO 题目标签是hashmap，需要思考考察啥？
+     * 参考别人解法，求的是从某一个节点，到其余节点，长度相同的所有节点之间双向连线
+     *
+     * @param points
+     * @return
+     */
+    public static int numberOfBoomerangs(int[][] points) {
+        int totalCount = 0;
+        for (int index1 = 0; index1 < points.length; index1++) {
+            Map<Integer, Integer> distanceMap = new HashMap<>();
+
+            for (int index2 = 0; index2 < points.length; index2++) {
+                if (index1 == index2) {
+                    continue;
+                }
+
+                int distance = (points[index1][0] - points[index2][0]) * (points[index1][0] - points[index2][0])
+                        + (points[index1][1] - points[index2][1]) * (points[index1][1] - points[index2][1]);
+                if (distanceMap.containsKey(distance)) {
+                    distanceMap.put(distance, distanceMap.get(distance) + 1);
+                } else {
+                    distanceMap.put(distance, 1);
+                }
+            }
+
+            for (Integer count : distanceMap.values()) {
+                totalCount += count * (count - 1);
+            }
+        }
+
+        return totalCount;
     }
 }
