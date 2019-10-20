f444814859328d1d1617bc54c30241659da066e4
13620613948@163.com <729399ren>
2019-10-21 00:39:10 +0800

    448

---
+++ src/main/java/algorithms/easy/E448_FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,31 @@
+package algorithms.easy;
+
+import java.util.List;
+
+/**
+ * Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+ * <p>
+ * Find all the elements of [1, n] inclusive that do not appear in this array.
+ * <p>
+ * Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+ * <p>
+ * Example:
+ * <p>
+ * Input:
+ * [4,3,2,7,8,2,3,1]
+ * <p>
+ * Output:
+ * [5,6]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E448_FindAllNumbersDisappearedInAnArray {
+    /**
+     * todo 我的想法是排序，时间复杂度为O(n*log(n))，不符合O(n)要求
+     */
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        return null;
+    }
+}
