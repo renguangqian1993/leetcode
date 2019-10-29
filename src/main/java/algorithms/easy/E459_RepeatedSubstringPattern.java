478e82713ba11216cd239ab71fb23ebf568136e4
renguangqian <13620613948@163.com>
2019-10-29 23:54:35 +0800

    todo

---
+++ src/main/java/algorithms/easy/E459_RepeatedSubstringPattern.java
@@ -0,0 +1,38 @@
+package algorithms.easy;
+
+/**
+ * Given a non-empty string
+ * check if it can be constructed by taking a substring of it
+ * and appending multiple copies of the substring together.
+ * You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
+ * <p>
+ *  
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: "abab"
+ * Output: True
+ * Explanation: It's the substring "ab" twice.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: "aba"
+ * Output: False
+ * <p>
+ * Example 3:
+ * <p>
+ * Input: "abcabcabcabc"
+ * Output: Tru
+ * <p>
+ * Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/repeated-substring-pattern
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E459_RepeatedSubstringPattern {
+    //TODO 减枝算法了解一下
+    public boolean repeatedSubstringPattern(String s) {
+        return false;
+    }
+}
