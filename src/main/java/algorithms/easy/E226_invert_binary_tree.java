ed09a4d2d442c3d03293233da042f5cab2b30fe8
renguangqian <guangqian.ren@renren-inc.com>
2020-09-16 16:15:35 +0800

    二叉树镜像反转

---
+++ src/main/java/algorithms/easy/E226_invert_binary_tree.java
@@ -0,0 +1,60 @@
+package algorithms.easy;
+
+import java.util.Objects;
+
+/**
+ * 226. 翻转二叉树
+ *
+ * 翻转一棵二叉树。
+ *
+ * 示例：
+ *
+ * 输入：
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 输出：
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ * 备注:
+ * 这个问题是受到 Max Howell 的 原问题 启发的 ：
+ *
+ * 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/invert-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E226_invert_binary_tree {
+
+    class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            if (!Objects.isNull(root)) {
+                TreeNode newLeft = invertTree(root.right);
+                TreeNode newRight = invertTree(root.left);
+                root.left = newLeft;
+                root.right = newRight;
+            }
+            return root;
+        }
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
