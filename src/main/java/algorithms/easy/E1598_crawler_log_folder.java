58ab3dff509d20f7e00a17f30dd6021ba37218be
renguangqian <13620613948@163.com>
2020-10-04 18:28:05 +0800

    栈

---
+++ src/main/java/algorithms/easy/E1598_crawler_log_folder.java
@@ -0,0 +1,53 @@
+package algorithms.easy;
+
+import java.util.Stack;
+
+/**
+ * 1598. 文件夹操作日志搜集器
+ *
+ * 每当用户执行变更文件夹操作时，LeetCode 文件系统都会保存一条日志记录。
+ *
+ * 下面给出对变更操作的说明：
+ *
+ * "../" ：移动到当前文件夹的父文件夹。如果已经在主文件夹下，则 继续停留在当前文件夹 。
+ * "./" ：继续停留在当前文件夹。
+ * "x/" ：移动到名为 x 的子文件夹中。题目数据 保证总是存在文件夹 x 。
+ * 给你一个字符串列表 logs ，其中 logs[i] 是用户在 ith 步执行的操作。
+ *
+ * 文件系统启动时位于主文件夹，然后执行 logs 中的操作。
+ *
+ * 执行完所有变更文件夹操作后，请你找出 返回主文件夹所需的最小步数 。
+ *
+
+ *
+ * 提示：
+ *
+ * 1 <= logs.length <= 103
+ * 2 <= logs[i].length <= 10
+ * logs[i] 包含小写英文字母，数字，'.' 和 '/'
+ * logs[i] 符合语句中描述的格式
+ * 文件夹名称由小写英文字母和数字组成
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/crawler-log-folder
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E1598_crawler_log_folder {
+    private class Solution {
+        public int minOperations(String[] logs) {
+            Stack<String> stack = new Stack<>();
+            for (String log : logs) {
+                if ("../".equals(log)) {
+                    if (!stack.isEmpty()) {
+                        stack.pop();
+                    }
+                } else if ("./".equals(log)) {
+                    continue;
+                } else {
+                    stack.push(log);
+                }
+            }
+            return stack.size();
+        }
+    }
+}
