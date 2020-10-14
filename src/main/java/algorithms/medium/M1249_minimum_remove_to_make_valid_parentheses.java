3609ca1a2e850816fe08358c079661902549f3a5
renguangqian <13620613948@163.com>
2020-10-14 13:17:35 +0800

    移除无效括号.单调栈

--- src/main/java/algorithms/medium/M1249_minimum_remove_to_make_valid_parentheses.java
+++ src/main/java/algorithms/medium/M1249_minimum_remove_to_make_valid_parentheses.java
@@ -49,9 +49,16 @@ import java.util.Stack;
 public class M1249_minimum_remove_to_make_valid_parentheses {
 
     public static void main(String[] args) {
-        new Solution().minRemoveToMakeValid("(a(b(c)d)");
+        new Solution2().minRemoveToMakeValid("lee(t(c)o)de)");
     }
-    private static class Solution {
+    /**
+     * 扫描字符串，字符入栈
+     * 统计左括号数量leftCount，遇到右括号时减一
+     * 多余的右括号，直接忽略不入栈
+     *
+     * 扫描结束后，拼接栈中字符，去掉leftCount个左括号
+     */
+    private static class Solution1 {
         public String minRemoveToMakeValid(String s) {
             int leftCount = 0;
             Stack<Character> stack = new Stack<>();
@@ -93,6 +100,55 @@ public class M1249_minimum_remove_to_make_valid_parentheses {
             }
 
             return new String(chars);
+        }
+    }
+
+    /**
+     * 使用单调栈 存储非法字符下标
+     * 第一次扫描字符串时从左到右，将非法坐标入栈
+     * 第二次扫描（组装结果）时从右到左，跳过非法坐标并弹出
+     */
+    private static class Solution2 {
+        public String minRemoveToMakeValid(String s) {
+
+            Stack<Integer> indexToRemove = new Stack<>();
+            for (int index = 0; index < s.length(); index++) {
+
+                switch (s.charAt(index)) {
+                    case '(': {
+                        indexToRemove.push(index);
+                        break;
+                    }
+                    case ')': {
+                        if (indexToRemove.isEmpty()) {
+                            //单独出现的右括号
+                            indexToRemove.push(index);
+                        } else if ('(' == s.charAt(indexToRemove.peek())) {
+                            //弹出成对出现的左括号
+                            indexToRemove.pop();
+                        } else {
+                            //单独出现的右括号
+                            indexToRemove.push(index);
+                        }
+                        break;
+                    }
+                    default: {
+                        //do nothing
+                        break;
+                    }
+                }
+            }
+
+            StringBuilder builder = new StringBuilder();
+            for (int index = s.length() - 1; index >= 0; index--) {
+                if (!indexToRemove.isEmpty() && index == indexToRemove.peek()) {
+                    indexToRemove.pop();
+                    continue;
+                }
+
+                builder.insert(0, s.charAt(index));
+            }
+            return builder.toString();
         }
     }
 }
